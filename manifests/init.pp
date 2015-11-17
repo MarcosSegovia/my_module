46b6bb37646fa7c7a3e17428bdce8515941c9fca
Marcos <velozmarkdrea@gmail.com>
2015-11-17 15:02:08 +0100

    adding new code from Pablo s repo

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,19 +3,27 @@ class my_module {
 
 	# HOSTS
 
-	host { 'mysql1':
+	host { 'localhost':
 		ensure => 'present',
 		target => '/etc/hosts',
 		ip => '127.0.0.1',
-		host_aliases => ['mysql']
+		host_aliases => ['mysql1']
 	}
 
-	host { 'memcached1':
+	host { 'localhost':
 		ensure => 'present',
 		target => '/etc/hosts',
 		ip => '127.0.0.1',
-		host_aliases => ['memcached']
+		host_aliases => ['memcached1']
 	}
+
+	# Miscellaneous packages.
+	$misc_packages = [
+		'sendmail','vim-enhanced','telnet','zip','unzip','screen',
+		'libssh2','libssh2-devel','gcc','gcc-c++','autoconf','automake','postgresql-libs'
+	]
+
+	package { $misc_packages: ensure => latest }
 
 	# APACHE
 
@@ -72,13 +80,13 @@ class my_module {
 		require => Yumrepo['remi-php56']
 	}
 
+	# MEMCACHED
+
+	php::module { [ 'devel', 'pear', 'xml', 'mbstring', 'pecl-memcache', 'soap' ]: }
+
 	# EPEL repo
 
 	include ::yum::repo::epel
-
-	# MEMCACHED
-
-	#class { 'memcached': }
 
 	# Files
 
@@ -86,12 +94,54 @@ class my_module {
 	    ensure  => 'present',
 	    content => "<h1>HEY I'm in the base folder !</h1>",
 	    mode    => '0644',
-  	}
+	}
 
-  file { '/var/www/project1/index.php':
-    ensure  => 'present',
-    content => "<h1>HEY I'm in the project1 folder !</h1>",
-    mode    => '0644',
-  }
+	file { '/var/www/project1/index.php':
+		ensure  => 'present',
+		content => "<h1>HEY I'm in the project1 folder !</h1>",
+		mode    => '0644',
+	}
+
+	# Ensure Time Zone and Region.
+	class { 'timezone':
+		timezone => 'Europe/Madrid',
+	}
+
+	#NTP
+	class { '::ntp':
+		server => [ '1.es.pool.ntp.org', '2.europe.pool.ntp.org', '3.europe.pool.ntp.org' ],
+	}
+
+	# Ip Tables.
+	if $operatingsystemrelease == '7.0.1406'
+	{
+		# firewalld - Centos 7
+		firewalld_rich_rule { 'Accept HTTP':
+		  ensure  => present,
+		  zone    => 'public',
+		  service => 'http',
+		  action  => 'accept',
+		}
+	}
+	else
+	{
+		package { 'iptables':
+		  ensure => present,
+		  before => File['/etc/sysconfig/iptables'],
+		}
+		file { '/etc/sysconfig/iptables':
+		  ensure  => file,
+		  owner   => "root",
+		  group   => "root",
+		  mode    => 600,
+		  replace => true,
+		  source  => "puppet:///modules/my_module/iptables.txt",
+		}
+		service { 'iptables':
+		  ensure     => running,
+		  enable     => true,
+		  subscribe  => File['/etc/sysconfig/iptables'],
+		}
+	}
 
 }
